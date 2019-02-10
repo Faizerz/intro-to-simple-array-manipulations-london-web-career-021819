41e0fd32261194545a0a31bf0429445815ba1f57
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 15:16:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,26 @@ end
 def using_insert(array, element)
   array[4].insert(element)
 end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  #
