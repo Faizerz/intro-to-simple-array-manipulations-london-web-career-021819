198d386c5a6c95e4c3467e730534f5803bb5d870
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 15:11:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,12 @@ def using_push(array, element)
   array.push(element)
 end
 
-def using_push(array, element)
-  array.push(element)
+def using_unshift(array, element)
+  array.unshift(element)
 end
+
+def using_pop(array, element)
+  array.pop(element)
+end
+
+def pop_with_args(array,element)
